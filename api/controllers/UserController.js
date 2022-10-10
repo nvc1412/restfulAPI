89977ce1707473f51f91feb211be22c6054b055b
cuong <nvc14122002@gmail.com>
2022-10-10 08:28:57 +0700

    update API user

---
+++ api/controllers/UserController.js
@@ -0,0 +1,46 @@
+"use strict";
+
+const util = require("util");
+const mysql = require("mysql");
+const db = require("../db");
+
+module.exports = {
+  get: (req, res) => {
+    let sql = "SELECT * FROM user";
+    db.query(sql, (err, response) => {
+      if (err) throw err;
+      res.json(response);
+    });
+  },
+  detail: (req, res) => {
+    let sql = "SELECT * FROM user WHERE email = ?";
+    db.query(sql, [req.params.userId], (err, response) => {
+      if (err) throw err;
+      res.json(response[0]);
+    });
+  },
+  update: (req, res) => {
+    let data = req.body;
+    let userId = req.params.userId;
+    let sql = "UPDATE user SET ? WHERE id = ?";
+    db.query(sql, [data, userId], (err, response) => {
+      if (err) throw err;
+      res.json({ message: "Update success!" });
+    });
+  },
+  store: (req, res) => {
+    let data = req.body;
+    let sql = "INSERT INTO user SET ?";
+    db.query(sql, [data], (err, response) => {
+      if (err) throw err;
+      res.json({ message: "Insert success!" });
+    });
+  },
+  delete: (req, res) => {
+    let sql = "DELETE FROM user WHERE id = ?";
+    db.query(sql, [req.params.userId], (err, response) => {
+      if (err) throw err;
+      res.json({ message: "Delete success!" });
+    });
+  },
+};
--- api/routes.js
+++ api/routes.js
@@ -1,13 +1,27 @@
+// "use strict";
+// module.exports = function (app) {
+//   let productsCtrl = require("./controllers/ProductsController");
+
+//   // todoList Routes
+//   app.route("/products").get(productsCtrl.get).post(productsCtrl.store);
+
+//   app
+//     .route("/products/:productId")
+//     .get(productsCtrl.detail)
+//     .put(productsCtrl.update)
+//     .delete(productsCtrl.delete);
+// };
+
 "use strict";
 module.exports = function (app) {
-  let productsCtrl = require("./controllers/ProductsController");
+  let userCtrl = require("./controllers/UserController");
 
   // todoList Routes
-  app.route("/products").get(productsCtrl.get).post(productsCtrl.store);
+  app.route("/user").get(userCtrl.get).post(userCtrl.store);
 
   app
-    .route("/products/:productId")
-    .get(productsCtrl.detail)
-    .put(productsCtrl.update)
-    .delete(productsCtrl.delete);
+    .route("/user/:userId")
+    .get(userCtrl.detail)
+    .put(userCtrl.update)
+    .delete(userCtrl.delete);
 };
