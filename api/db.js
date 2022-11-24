eec43876ee394d4e81f5b1d7e6f6144637e76178
cuong <nvc14122002@gmail.com>
2022-11-24 17:42:21 +0700

    update db

--- api/db.js
+++ api/db.js
@@ -13,7 +13,21 @@
 "use strict";
 const mysql = require("mysql");
 
-var pool = mysql.createPool({
+// var pool = mysql.createPool({
+//   host: process.env.DB_HOST || "37.59.55.185",
+//   user: process.env.DB_USER || "NrFhfQnJEc",
+//   password: process.env.DB_PASS || "MeYA0OwtS9",
+//   port: process.env.PORT || 3306,
+//   database: process.env.DB_NAME || "NrFhfQnJEc",
+// });
+
+// pool.query("SELECT 1 + 1 AS solution", function (error, results, fields) {
+//   if (error) throw error;
+//   console.log("The solution is: ");
+// });
+// module.exports = pool;
+
+const db = mysql.createConnection({
   host: process.env.DB_HOST || "37.59.55.185",
   user: process.env.DB_USER || "NrFhfQnJEc",
   password: process.env.DB_PASS || "MeYA0OwtS9",
@@ -21,18 +35,4 @@ var pool = mysql.createPool({
   database: process.env.DB_NAME || "NrFhfQnJEc",
 });
 
-pool.query("SELECT 1 + 1 AS solution", function (error, results, fields) {
-  if (error) throw error;
-  console.log("The solution is: ");
-});
-module.exports = pool;
-
-// const db = mysql.createConnection({
-//   host: "37.59.55.185",
-//   user: "NrFhfQnJEc",
-//   password: "MeYA0OwtS9",
-//   port: 3306,
-//   database: "NrFhfQnJEc",
-// });
-
-// module.exports = db;
+module.exports = db;
