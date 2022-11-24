a6c4eb774a4ae8d878d287b4bfb993f5aff65d02
cuong <nvc14122002@gmail.com>
2022-11-24 18:40:43 +0700

    update

--- api/db.js
+++ api/db.js
@@ -9,30 +9,3 @@ const db = mysql.createConnection({
 });
 
 module.exports = db;
-
-//"use strict";
-//const mysql = require("mysql");
-
-// var pool = mysql.createPool({
-//   host: process.env.DB_HOST || "37.59.55.185",
-//   user: process.env.DB_USER || "NrFhfQnJEc",
-//   password: process.env.DB_PASS || "MeYA0OwtS9",
-//   port: process.env.PORT || 3306,
-//   database: process.env.DB_NAME || "NrFhfQnJEc",
-// });
-
-// pool.query("SELECT 1 + 1 AS solution", function (error, results, fields) {
-//   if (error) throw error;
-//   console.log("The solution is: ");
-// });
-// module.exports = pool;
-
-// const db = mysql.createConnection({
-//   host: process.env.DB_HOST || "37.59.55.185",
-//   user: process.env.DB_USER || "NrFhfQnJEc",
-//   password: process.env.DB_PASS || "MeYA0OwtS9",
-//   port: process.env.PORT || 3306,
-//   database: process.env.DB_NAME || "NrFhfQnJEc",
-// });
-
-// module.exports = db;
